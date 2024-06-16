0893d6c950f45f5abd081445c56aca52a2d071fc
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-06-17 00:03:19 +0530

    updated

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,13 +1,17 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function Page() {
 	const user = await currentUser();
+	if (!user) return null; // to avoid typescript warnings
 
-	const userInfo = {};
+	const userInfo = await fetchUser(user.id);
+	if (userInfo?.onboarded) redirect("/");
 
 	const userData = {
-		id: user?.id,
+		id: user.id,
 		objectId: userInfo?.id,
 		username: userInfo?.username || user?.username,
 		name: userInfo?.name || user?.firstName || "",
